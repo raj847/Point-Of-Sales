f4d63ce0cdab2710638fe8fd2f8356ae56608f5c
Sammi Aldhi Yanto <sammidev4@gmail.com>
2023-02-23 19:27:06 +0700

    edit auth

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,7 +3,10 @@ package middleware
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 	"vandesar/entity"
 
 	"github.com/dgrijalva/jwt-go"
@@ -11,22 +14,35 @@ import (
 
 func MustAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		headerType := r.Header.Get("Content-Type")
-		c, err := r.Cookie("user_id")
+		// headerType := r.Header.Get("Content-Type")
 
-		if err != nil {
-			if headerType == "application/json" {
-				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode(entity.NewErrorResponse("error unauthorized user id"))
-				return
-			} else {
-				http.Redirect(w, r, "/login", http.StatusSeeOther)
-				return
-			}
+		authorizationHeader := r.Header.Get("Authorization")
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header format")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(entity.NewErrorResponse(err.Error()))
+			return
 		}
+
+		token := fields[1]
+		fmt.Println(token)
+		// c, err := r.Cookie("user_id")
+
+		// if err != nil {
+		// 	if headerType == "application/json" {
+		// 		w.WriteHeader(http.StatusUnauthorized)
+		// 		json.NewEncoder(w).Encode(entity.NewErrorResponse("error unauthorized user id"))
+		// 		return
+		// 	} else {
+		// 		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		// 		return
+		// 	}
+		// }
 		claims := &entity.Claims{}
 
-		tkn, err := jwt.ParseWithClaims(c.Value, claims, func(t *jwt.Token) (interface{}, error) {
+		tkn, err := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
 			return []byte("rahasia-perusahaan"), nil
 		})
 
